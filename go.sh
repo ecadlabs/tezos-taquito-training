27ba6cb23cf76ca5033c712e2fa8f2db139b80f8
Edward Garson <egarson@gmail.com>
2023-10-26 10:30:26 -0700

    Make `source go.sh` work from any directory (fixup)

--- go.sh
+++ go.sh
@@ -4,28 +4,31 @@
 #
 # * Ensure you are using `zsh` (you can check with `$ echo $SHELL`)
 # * Run `$ source go.sh` to load the commands into your current shell/session
+#
+# Methods prefixed with `_` are 'pseudo-internal' / not really for direct usage.
 
-source ./util.sh
+CURRENT_DIRECTORY=${0:a:h}  # zsh magic
+source ${CURRENT_DIRECTORY}/util.sh  # Make `source go.sh` work from any directory
 
 # TODO Trial `devbox` for this?
 setup_node() {
-    [[ $(command -v nvm) ]] || echo 'No `nvm` found: Ensure you run Node v16.16+'
+    [[ $(command -v nvm) ]] || { _err 'No `nvm` found: Ensure you run Node v16.16+'; return 1 }
     # NOTE Node v17 is not supported by Taquito!
     nvm use v16
 }
 
 convert_mermaid_diagrams_to_svg () {
     # `npm install -g mermaid.cli`
-	  [[ -n $(command -v mmdc) ]] || { _err '`Mermaid` has not been installed' && return 1 }
+	  [[ -n $(command -v mmdc) ]] || { _err '`Mermaid` has not been installed'; return 1 }
 	  for diagram in img/**/*.mmd; do
 		    mmdc -i $diagram -o ${diagram:r}.svg
 	  done
 }
 
 _convert_to_xyz() {
-    [[ -f $1 ]] || { _err "File not found: $1"; return 2 }
+    [[ -f $1 ]] || { _err "File not found: $1"; return 1 }
     # `sudo apt get install imagemagick`
-    [[ -n $(command -v mogrify) ]] || { _err '`ImageMagick` has not been installed'; return 3 }
+    [[ -n $(command -v mogrify) ]] || { _err '`ImageMagick` has not been installed'; return 2 }
     mogrify -format $2 -resize 1920x1080 -density 300 ${1:r}.${2}
 }
 
