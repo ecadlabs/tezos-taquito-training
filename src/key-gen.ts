de964f48718304c756acecee38bc5a4002869724
huianyang <hui-an.yang@ecadlabs.com>
2023-11-03 15:28:46 -0700

    updated keygen to tz1 and minor changes

--- src/key-gen.ts
+++ src/key-gen.ts
@@ -12,13 +12,13 @@ import nodeCrypto from 'crypto';
     publicKey: await signer.publicKey(),
     secretKey: secretKey,
   };
-  await writeFile('key.json', JSON.stringify(key));
-  await writeFile('.env.dev', `PKH=${key.publicKeyHash}\nSECRET_KEY=${key.secretKey}\nCONTRACT_ADDRESS=KT1DY97sFj5TZY7s3BhMeydRSV7PS19nzxAG\nRPC=https://ghostnet.ecadinfra.com\n`);
+  await writeFile('key.json', JSON.stringify(key, null, 2));
+  await writeFile('.env.dev', `PUBLIC_KEY_HASH=${key.publicKeyHash}\nSECRET_KEY=${key.secretKey}\nCONTRACT_ADDRESS=KT1DY97sFj5TZY7s3BhMeydRSV7PS19nzxAG\nRPC=https://ghostnet.ecadinfra.com\n`);
 })();
 
-// tz3 prefix publicKeyHash(address)
+// tz1 prefix publicKeyHash(address)
 function genSecretKey() {
   const keyBytes = Buffer.alloc(32);
   nodeCrypto.randomFillSync(keyBytes);
-  return b58cencode(new Uint8Array(keyBytes), prefix[Prefix.P2SK]);
+  return b58cencode(new Uint8Array(keyBytes), prefix[Prefix.EDSK2]);
 };
